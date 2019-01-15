0739e2a0290e6c3b119bf1eb83e1d1918ee51a17
Dougpoole333 <dougpoole333@gmail.com>
2019-01-15 15:17:39 -0500

    renderscreen render function maps props.letters array to an array of letter components

--- p2/ransom_note/src/Components/RenderScreen.js
+++ p2/ransom_note/src/Components/RenderScreen.js
@@ -2,18 +2,16 @@ import React, { Component } from "react"
 import Letter from "./Letter.js"
 
 class RenderScreen extends Component{
-
-
-
+    
     render(){
         const letterComponents = this.props.letters.map((letter, i) => {
-            return(
-                <
-            )
+            if (letter !== " "){
+                return(<Letter key={i} letter={letter} />)
+            } else {return (<div key={i} className="space"></div>)}
         })
         return(
             <div className="render-screen">
-                
+                {letterComponents}
             </div>
         )
     }
