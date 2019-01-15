95095c369dade704643a1db045cf6c4dc2a4964f
Dougpoole333 <dougpoole333@gmail.com>
2019-01-15 17:06:23 -0500

    added flex styling to render screen, added state and inline styles to letter component

--- p2/ransom_note/src/Components/Letter.js
+++ p2/ransom_note/src/Components/Letter.js
@@ -1,11 +1,31 @@
-import React from "react"
+import React, { Component } from "react"
 
-function Letter(props){
-    return(
-        <div className="letter">
-            {props.letter}
-        </div>
-    )
+class Letter extends Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            color: "",
+            backgroundColor: "",
+        }
+    }
+    
+
+
+    render(){
+        const letterStyle = {
+            backgroundColor: "pink",
+            padding: "5px",
+            margin: "auto 5px",
+            fontSize: "40px",
+        }
+
+        return(
+            <div style={letterStyle}>
+                {this.props.letter}
+            </div>
+        )
+    }
+    
 }
 
 export default Letter
