938af80a2598e65c14c13104a255cd85c71eb64c
Dougpoole333 <dougpoole333@gmail.com>
2019-01-15 13:32:10 -0500

    created user input component

--- p2/ransom_note/src/App.js
+++ p2/ransom_note/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import UserInput from "./UserInput.js"
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Ransom Note</h1>
+        <UserInput />
       </div>
     );
   }
---
+++ p2/ransom_note/src/UserInput.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function UserInput(){
+    return(
+        <form>
+            <input type="text" name="input" value="Your Message Here"/>
+            <input type="submit" value="Render"/>
+        </form>    
+    )
+}
+
+export default UserInput
