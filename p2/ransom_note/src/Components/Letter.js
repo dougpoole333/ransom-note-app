0a1b4f197821cef46b53f32dc0748d9f03057493
Dougpoole333 <dougpoole333@gmail.com>
2019-01-15 20:38:01 -0500

    added some random styling methods to letter component

--- p2/ransom_note/src/Components/Letter.js
+++ p2/ransom_note/src/Components/Letter.js
@@ -4,23 +4,62 @@ class Letter extends Component{
     constructor(props){
         super(props)
         this.state ={
-            color: "",
-            backgroundColor: "",
+                backgroundColor: "white",
+                padding: "5px 5px",
+                margin: "5px 5px",
+                fontSize: "",
+                fontStyle: "",
+                textTransform: "",
+                color: ""
         }
     }
+
+    returnRandomColor = () => {
+        let r=Math.floor(Math.random()*255)
+        let g=Math.floor(Math.random()*255)
+        let b=Math.floor(Math.random()*255)
+            return `rgb(${r},${g},${b})`
+    }
+
+    returnRandomGrayscale = () => {
+        let n=Math.floor(Math.random()*255)
+            return `rgb(${n},${n},${n})`
+    }
+
+    returnRandomFontColor = () => {
+        let array = [this.returnRandomColor(), this.returnRandomGrayscale()]
+        return array[Math.floor(Math.random()*2)]
+    }
+
+    returnRandomFontSize = () => {
+        let n = Math.floor(Math.random()*40) + 20
+        return n + "px"
+    }
     
+    returnRandomFontStyle = () => {
+        let array = ["normal", "italic", "bold"];
+        return array[Math.floor(Math.random()*3)]
+    }
+
+    returnRandomTextTransform = () => {
+        let array = ["uppercase","lowercase"]
+        return array[Math.floor(Math.random()*2)]
+    }
+
+
+    componentDidMount(){
+        this.setState({
+            fontSize: this.returnRandomFontSize(),
+            color: this.returnRandomFontColor(),
+            fontStyle: this.returnRandomFontStyle(),
+            textTransform: this.returnRandomTextTransform()
+        })
+    }
 
 
     render(){
-        const letterStyle = {
-            backgroundColor: "pink",
-            padding: "5px",
-            margin: "auto 5px",
-            fontSize: "40px",
-        }
-
         return(
-            <div style={letterStyle}>
+            <div style={this.state}>
                 {this.props.letter}
             </div>
         )
