bb9f8bc06ba437ea7a3f676aec5a614c65a86522
Dougpoole333 <dougpoole333@gmail.com>
2019-01-16 21:08:34 -0500

    styled info display, added conditional rendering and toggle display method

--- p2/ransom_note/src/Components/InfoDisplay.js
+++ p2/ransom_note/src/Components/InfoDisplay.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react"
 
 class InfoDisplay extends Component{
+    constructor(props){
+        super()
+        this.state = {
+            open: false
+        }
+    }
+    toggleDisplayOpen = () => {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+
     render(){
         return(
-            <div className="info-display">
-                <h1>info goes here</h1>
+            <div
+                className="info-display"
+                style={this.state.open ? {bottom: "0"} : {bottom: "-200px"}}>
+                <h1 
+                    className="info-header"
+                    onClick={this.toggleDisplayOpen}>STYLE INFO</h1>
             </div>
         )
     }
